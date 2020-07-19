cbcfaa916b786ed0f31f655946a939b9dfc676cf
LX <1229312744@qq.comm>
2020-07-19 20:03:42 +0800

    formctronl and formGroup

---
+++ demos-form/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { NameEditorComponent } from './name-editor/name-editor.component';
+import { ReactiveFormsModule, FormsModule} from '@angular/forms';
+import { ProfileEditorComponent } from './profile-editor/profile-editor.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NameEditorComponent,
+    ProfileEditorComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
