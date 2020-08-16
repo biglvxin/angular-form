5d56ffd63ee1180743afc89c5e339acc3e75e688
LX <1229312744@qq.comm>
2020-08-16 19:40:05 +0800

    formArray at removAt push

--- demos-form/src/app/form-nested/form-nested.component.ts
+++ demos-form/src/app/form-nested/form-nested.component.ts
@@ -21,12 +21,29 @@ export class FormNestedComponent implements OnInit {
           stageName:[null, [Validators.required]],
           stageType: [null, [Validators.required]],
           stageContent: this.fb.array([
-            this.fb.control(null)
+            // this.fb.control(null)
+            this.fb.group({
+              stepName: [null, [Validators.required]],
+              stepType: [null, [Validators.required]],
+              stepContent: this.fb.array([
+                this.fb.control(null)
+              ])
+            })
           ])
         })
       ])
     });
     console.log(this.validateForm);
+  }
+  addStep(workflowIndex: number) {
+    (this.workFlowContent.at(workflowIndex).get('stageContent') as FormArray).push(
+      this.fb.group({
+        stepName: [null, [Validators.required]],
+        stepType: [null, [Validators.required]],
+        stepContent: this.fb.array([
+          this.fb.control(null)
+        ])
+      }));
   }
   addStage() {
     this.workFlowContent.push(
@@ -34,7 +51,43 @@ export class FormNestedComponent implements OnInit {
         stageName: [null, [Validators.required]],
         stageType: [null, [Validators.required]],
         stageContent: this.fb.array([
-          this.fb.control(null)
+          // this.fb.control(null)
+          this.fb.group({
+            stepName: [null, [Validators.required]],
+            stepType: [null, [Validators.required]],
+            stepContent: this.fb.array([
+              this.fb.control(null)
+            ])
+          })
+        ]),
+      })
+    );
+  }
+  addStepContent(stage: FormGroup) {
+    (stage.get('stepContent') as FormArray).push(this.fb.control(null));
+  }
+  removeStage(workflowIndex: number) {
+      this.workFlowContent.removeAt(workflowIndex);
+  }
+  removeStep(workflowIndex: number, stageIndex: number) {
+    (this.workFlowContent.at(workflowIndex).get('stageContent') as FormArray).removeAt(stageIndex);
+  }
+  removeStepContent(stage: FormGroup, stepIndex: number) {
+    (stage.get('stepContent') as FormArray).removeAt(stepIndex);
+  }
+  addWorkFlow(): void {
+    this.workFlowContent.push(
+      this.fb.group({
+        stageName: [null, [Validators.required]],
+        stageType: [null, [Validators.required]],
+        stageContent: this.fb.array([
+          this.fb.group({
+            stepName: [null, [Validators.required]],
+            stepType: [null, [Validators.required]],
+            stepContent: this.fb.array([
+              this.fb.control(null)
+            ])
+          })
         ]),
       })
     );
